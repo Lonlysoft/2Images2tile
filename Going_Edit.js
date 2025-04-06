d17e872e8366e4a7a292e69cbcc9121ce81ac0fe
Lonlysoft <nopuphere@gmail.com>
2025-04-06 00:35:40 -0300

    update

---
+++ Going_Edit.js
@@ -0,0 +1,206 @@
+const canvas = document.getElementById("editor");
+const ctx = canvas.getContext("2d");
+const canvas_template = document.getElementById("paleta");
+const ctx_template = canvas.getContext("2d");
+
+const coresTemplate = [];
+const mapa = [];
+
+function obterCorDoPixel(context, x, y) {
+	let pixel = context.getImageData(x, y, 1, 1);
+	let cor = "rgba(" + pixel.data[0] + ", " + pixel.data[1] + ", " + pixel.data[2] + "," + pixel.data[3]+")";
+	return cor;
+}
+
+function obterCorDeTemplate(temp_imagery){
+	ctx_template.drawImage(temp_imagery, 0, 0);
+	for(let i = 0; i < temp_imagery.width; i+=1){
+		coresTemplate.push(obterCorDoPixel(ctx_template, i, 0));
+	}
+}
+const osInputs = {
+	tipoDeSetup: document.querySelector(".input__image_grid"),
+	asCoresEmOrdem: document.querySelector(".input__image_template")
+};
+osInputs.tipoDeSetup.addEventListener("change", handleImagery);
+osInputs.asCoresEmOrdem.addEventListener("change", handlePallet);
+
+function idOf(array, contents){
+	if(!array.includes(contents)){
+		return -1;
+	}
+	for(let i = 0; i < array.length; i++){
+		if(contents == array[i])
+			return i;
+	}
+}
+
+
+
+function general(imagemSdw){
+	let corDoPixel = []
+	ctx.drawImage(imagemSdw, 0, 0, imagemSdw.width, imagemSdw.height);
+	for(let i = 0; i < imagemSdw.height; i++){
+		corDoPixel.push(new Array());
+		mapa.push(new Array());
+		for(let j = 0; j < imagemSdw.width; j++){
+			corDoPixel[i].push(obterCorDoPixel(ctx, j, i));
+			mapa[i].push(idOf(coresTemplate, corDoPixel[i][j]));
+		}
+	}
+}
+
+const TileSize = 48;
+
+function generalForGraphicPurposes(imagemSdw){
+	let corDoPixel = []
+	let imageLengthInTileSize = Math.floor(imagemSdw.width/TileSize);
+	ctx.drawImage(imagemSdw, 0, 0, imagemSdw.width, imagemSdw.height);
+	for(let i = 0; i < imagemSdw.height; i++){
+		mapa.push(new Array());
+		corDoPixel.push(new Array());
+		for(let j = 0; j < imagemSdw.width; j++){
+			corDoPixel[i].push(obterCorDoPixel(ctx, j, i));
+			let fret = 5 + idOf(coresTemplate, corDoPixel[i][j]);
+			mapa[i].push(fret*4);
+		}
+	}
+	for(let i = 0; i < mapa.length; i++){
+		for(let j = 0; j < mapa[i].length; j++){
+			let flag = [false, false, false, false];
+			if((i > 0 && j > 0) && (i < imagemSdw.height-1 && j < imagemSdw.width-1)){
+				if(corDoPixel[i - 1][j] != corDoPixel[i][j])
+					flag[0] = true;
+				if(corDoPixel[i + 1][j] != corDoPixel[i][j])
+					flag[1] = true;
+				if(corDoPixel[i][j - 1] != corDoPixel[i][j])
+					flag[2] = true;
+				if(corDoPixel[i][j + 1] != corDoPixel[i][j])
+					flag[3] = true;
+			}
+			
+			if(flag[0] && flag[1] && flag[2] && flag[3])
+				mapa[i][j] -= imageLengthInTileSize*2+2;
+			
+			else if(flag[0] && flag[1] && flag[2] && !flag[3])
+				mapa[i][j] += imageLengthInTileSize*2 -1;
+			
+			else if(flag[0] && flag[1] && flag[3] && !flag[2])
+				mapa[i][j] += imageLengthInTileSize*2 +1;
+				
+			else if(flag[2] && flag[3] && flag[0] && !flag[1])
+				mapa[i][j] -= imageLengthInTileSize+2;
+				
+			else if(flag[2] && flag[3] && flag[1] && !flag[0])
+				mapa[i][j] += imageLengthInTileSize+2;
+			
+			else if(flag[0] && flag[1] && !flag[2] && !flag[3])
+				mapa[i][j] -= imageLengthInTileSize*2;
+				
+			else if(flag[0] && flag[2] && !flag[1] && !flag[3])
+				mapa[i][j] -= imageLengthInTileSize-1;
+				
+			else if(flag[0] && flag[3] && !flag[1] && !flag[2])
+				mapa[i][j] -= imageLengthInTileSize+1;
+				
+			else if(flag[1] && flag[2] && !flag[0] && !flag[3])
+				mapa[i][j] += imageLengthInTileSize-1;
+				
+			else if(flag[1] && flag[3] && !flag[0] && !flag[2])
+				mapa[i][j] += imageLengthInTileSize+1;
+				
+			else if(flag[2] && flag[3] && !flag[1] && !flag[0])
+				mapa[i][j] += 2;
+			
+			else if(flag[0] && !flag[1] && !flag[2] && !flag[3])
+				mapa[i][j] -= imageLengthInTileSize;
+				
+			else if(flag[1] && !flag[0] && !flag[2] && !flag[3])
+				mapa[i][j] += imageLengthInTileSize;
+				
+			else if(flag[2] && !flag[0] && !flag[1] && !flag[3])
+				mapa[i][j]--;
+				
+			else if(flag[3] && !flag[0] && !flag[1] && !flag[2])
+				mapa[i][j]++;
+		}
+	}
+}
+
+let image;
+let pallet;
+
+const frontImage = document.querySelector(".main_img");
+const backImage = document.querySelector(".pallet_img");
+
+function handleImagery(e){
+	const arq = e.target.files[0];
+	saveImage(arq);
+}
+
+function handlePallet(e){
+	const arq = e.target.files[0];
+	savePallet(arq);
+	
+}
+
+function saveImage(file){
+	const leitor = new FileReader();
+	leitor.onload = function(e){
+		image = new Image();
+		image.src = e.target.result;
+		frontImage.classList.remove("none");
+		frontImage.src = image.src;
+	}
+	leitor.readAsDataURL(file);
+}
+
+function savePallet(file){
+	const leitor = new FileReader();
+	leitor.onload = function(e){
+		pallet = new Image();
+		pallet.src = e.target.result
+		backImage.classList.remove("none");
+		backImage.src = pallet.src;
+	}
+	leitor.readAsDataURL(file);
+}
+
+const map__type = document.querySelector(".map__type");
+const output = document.querySelector(".output");
+
+let finalString = "[";
+function calculate_andSend(){
+	let send = document.querySelector(".sendBtn");
+	send.style.background = "var(--p-color)";
+	send.innerHTML = "processando...";
+	window.setTimeout(()=>{
+		canvas.width = image.width;
+		canvas.height = image.height;
+		canvas_template.width = pallet.width;
+		canvas_template.height = pallet.height;
+		obterCorDeTemplate(pallet);
+		if(map__type.value == "relevo"){
+			general(image);
+		}
+		else {
+			generalForGraphicPurposes(image)
+		}
+		for(let i = 0; i < canvas.height; i++){
+			finalString += "["
+			for(let j = 0; j < canvas.width; j++){
+				finalString += (mapa[i][j]);
+				finalString += ", ";
+			}
+			finalString += "], <br/>" 
+		}
+		finalString += "],"
+		send.innerHTML = "pronto!"; output.classList.remove("none");
+	}, 1000);
+}
+
+
+function download(){
+	const blob = new Blob([finalString], {type: "text/plain;charset=utf-8"})
+	saveAs(blob, "sector.txt")
+}
---
+++ README.md
@@ -0,0 +1,13 @@
+# O que é esse projeto?
+
+esse foi um sub-projeto que eu por algum motivo fiz para fazer tilesets mais rápido.
+
+Olhar problemas inuteis
+-
+
+um dos problemas que eu percebi enquanto eu fazia o funky pack era que muitas das minhas tilesets que eu queria fazer iriam demorar para ficarem prontas se eu quisesse fazer digitando numero por número. Então veio a ideia de fazer um programa que criasse esses tilesets de forma mais automática a partir de algo que eu tenho mais controle. Como Imagens.
+
+Por que eu fiz isso ao invés de usar o tiled ou qualquer outro app/software que monta os tilesets de uma forma normal?
+-
+
+A Resposta é bem mais simples: Eu não tenho como fazer isso no celular. O tiled não existe para celulares então exportar uma matiz se torna praticamente impossivel, sendo as únicas opições escrever manualmente ou programar o seu proprio projetinho pra automatizar as paradas.
